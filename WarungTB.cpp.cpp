70b345734a30c5fcef6f18065bab772f516e5dc9
revaldo <aldo110606@gmail.com>
2024-10-03 17:18:43 +0700

    Add files via upload

---
+++ WarungTB.cpp.cpp
@@ -0,0 +1,12 @@
+/*-----------------------------------
+Selamat Datang di Warung Tiga Belas (TB).
+-------------------------------------*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Selamat Datang di Warung TB";
+
+    return 0;
+}
