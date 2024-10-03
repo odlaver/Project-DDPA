a78358c389fee462c2ddbb179a3c7bcf3b6c400b
revaldo <aldo110606@gmail.com>
2024-10-03 23:01:34 +0700

    update WarungTB.cpp

--- WarungTB.cpp
+++ WarungTB.cpp
@@ -1,29 +1,30 @@
-/*=======================================
+/*-----------------------------------
 Selamat Datang di Warung Tiga Belas (TB).
-=========================================*/
+-------------------------------------*/
 #include <iostream>
 using namespace std;
 
 //reval menambahkan data Warung
 const int jumlahJajanan = 10;
-  string jajanan[jumlahJajanan] = {"Beng Beng", "Coklatos", "Better", "Nabati", "Basreng", "Chitato", "Taroo", "Golda", "Fruit Tea", "Aqua"};
-  int hargaJajanan[jumlahJajanan] = {2500, 1000, 2000, 2000, 500, 2000, 2000, 3000, 5000, 5000};
+string jajanan[jumlahJajanan] = {"Beng Beng", "Coklatos", "Better", "Nabati", "Basreng", "Chitato", "Taroo", "Golda", "Fruit Tea", "Aqua"};
+int hargaJajanan[jumlahJajanan] = {2500, 1000, 2000, 2000, 500, 2000, 2000, 3000, 5000, 5000};
 
- void tampilkanMenu(){
-    cout << "\n========= MENU JAJANAN =========\n";
+//menu jajan dan harga
+void tampilkanMenu(){
+    cout << "\n=========== MENU  JAJANAN ===========\n";
     for(int i = 0; i < jumlahJajanan; i++){
         cout << i+1 << ". " << jajanan[i] << "\t - \t" << "Rp. " << hargaJajanan[i] << endl;
     }
-    cout << "================================" << endl;
+    cout << "=====================================" << endl;
  }
 
 //reval menambahkan total pesanan dan struk belanja
 void totalPesanan(int pilihan[], int jumlah[], int jumlahPesanan){
     int totalHarga = 0;
     
-    cout << "\n========= STRUK BELANJA =========\n";
-    cout << "Barang\t\tJumlah\tHarga\n";
-    cout << "===============================\n";
+    cout << "\n=========== STRUK BELANJA ===========\n";
+    cout << "Barang\t\tJumlah\t\tHarga\n";
+    cout << "=====================================\n";
     
     for(int i = 0; i < jumlahPesanan; i++){
         int indexJajanan = pilihan[i] - 1;
@@ -32,16 +33,13 @@ void totalPesanan(int pilihan[], int jumlah[], int jumlahPesanan){
         cout << jajanan[indexJajanan] << "\t" << jumlah[i] << "\tRp. " << harga << endl;
     }
     
-    cout << "===========================\n";
+    cout << "_____________________________________\n";
     cout << "Total Harga: Rp. " << totalHarga << endl;
-    cout << "===========================\n";
+    cout << "=====================================\n";
+    
 }
 
- int main(){
-    int pilihan;
-    tampilkanMenu();
-    cout << "Masukkan pilihan Kalian : ";
-    cin >> pilihan;
+int main(){
 
     return 0;
- }
+}
