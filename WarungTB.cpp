f25398023dc4bf5ec22692a2e5b27df6b312b145
revaldo <aldo110606@gmail.com>
2024-10-03 19:19:20 +0700

    reval menambah data WarungTB.cpp

--- WarungTB.cpp
+++ WarungTB.cpp
@@ -4,6 +4,11 @@ Selamat Datang di Warung Tiga Belas (TB).
 #include <iostream>
 using namespace std;
 
+//reval menambahkan data Warung
+const int jumlahJajanan = 10;
+string jajanan[jumlahJajanan] = {Beng Beng, Coklatos, Better, Nabati, Basreng, Chitato, Taro, Golda, Fruit Tea, Aqua};
+int harga[jumlahJajanan] = {2500, 1000, 2000, 2000, 500, 2000, 2000, 3000, 5000, 5000};
+
 int main()
 {
     cout << "Selamat Datang di Warung TB";
