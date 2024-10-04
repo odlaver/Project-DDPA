93a7d9b8e811fc5d7ec15c0d1f8027c60fc7e369
revaldo <aldo110606@gmail.com>
2024-10-04 20:57:40 +0700

    revisi desain WarungTB.cpp

--- WarungTB.cpp
+++ WarungTB.cpp
@@ -6,37 +6,37 @@ using namespace std;
 
 //reval menambahkan data Warung
 const int jumlahJajanan = 10;
-string jajanan[jumlahJajanan] = {"Beng Beng", "Coklatos", "Better", "Nabati", "Basreng", "Chitato", "Taroo", "Golda", "Fruit Tea", "Aqua"};
+string jajanan[jumlahJajanan] = {"BengBeng", "Coklatos", "Better  ", "Nabati  ", "Basreng  ", "Chitato ", "Taroo   ", "Golda   ", "FruitTea", "Aqua    "};
 int hargaJajanan[jumlahJajanan] = {2500, 1000, 2000, 2000, 500, 2000, 2000, 3000, 5000, 5000};
 
 //menu jajan dan harga
-void tampilkanMenu(){
-    cout << "\n=========== MENU  JAJANAN ===========\n";
-    for(int i = 0; i < jumlahJajanan; i++){
-        cout << i+1 << ". " << jajanan[i] << "\t - \t" << "Rp. " << hargaJajanan[i] << endl;
-    }
-    cout << "=====================================" << endl;
- }
+void tampilkanMenu() {
+	cout << "\n=========== MENU  JAJANAN ===========\n";
+	for(int i = 0; i < jumlahJajanan; i++) {
+		cout << i + 1 << ". " << jajanan[i] << "\t - \t" << "Rp. " << hargaJajanan[i] << endl;
+	}
+	cout << "=====================================" << endl;
+}
 
 //reval menambahkan total pesanan dan struk belanja
-void totalPesanan(int pilihan[], int jumlah[], int jumlahPesanan){
-    int totalHarga = 0;
-    
-    cout << "\n=========== STRUK BELANJA ===========\n";
-    cout << "Barang\t\tJumlah\t\tHarga\n";
-    cout << "=====================================\n";
-    
-    for(int i = 0; i < jumlahPesanan; i++){
-        int indexJajanan = pilihan[i] - 1;
-        int harga = hargaJajanan[indexJajanan] * jumlah[i];
-        totalHarga += harga;
-        cout << jajanan[indexJajanan] << "\t" << jumlah[i] << "\tRp. " << harga << endl;
-    }
-    
-    cout << "_____________________________________\n";
-    cout << "Total Harga: Rp. " << totalHarga << endl;
-    cout << "=====================================\n";
-    
+int totalPesanan(int pilihan[], int jumlah[], int jumlahPesanan) {
+	int totalHarga = 0;
+
+	cout << "\n=========== STRUK BELANJA ===========\n";
+	cout << "Barang \t\tJumlah \t\tHarga\n";
+	cout << "=====================================\n";
+
+	for(int i = 0; i < jumlahPesanan; i++) {
+		int indexJajanan = pilihan[i] - 1;
+		int harga = hargaJajanan[indexJajanan] * jumlah[i];
+		totalHarga += harga;
+		cout << jajanan[indexJajanan] << "\t" << jumlah[i] << "\t    Rp. " << harga << endl;
+	}
+
+	cout << "=====================================\n";
+	cout << "Total Harga: \tRp. " << totalHarga << endl;
+	cout << "=====================================\n";
+	return totalHarga;
 }
 
 int main(){
